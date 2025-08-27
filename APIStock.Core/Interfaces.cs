6005124d3391160d43507610cb3b5feeb90e054e
wachy92-source <wachy92@gmail.com>
2025-08-27 12:03:05 -0600

    Add files via upload

---
+++ APIStock.Core/Interfaces.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using APIStock.Core.Entities;
+
+namespace APIStock.Core.Interfaces
+{
+    public interface IStockRepository
+    {
+        Task<List<StockItem>> GetStockAsync();
+    }
+}
---
+++ APIStock.Core/StockItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIStock.Core.Entities
+{
+    public class StockItem
+    {
+        public int Id { get; set; }
+        public string Estilo { get; set; }
+        public string Color { get; set; }
+        public int Total { get; set; }
+        public string Talla { get; set; }
+    }
+}
+
