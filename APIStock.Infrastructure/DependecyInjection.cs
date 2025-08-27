d1d77d83871d04ad44368f3b3e9863b7020f272c
wachy92-source <wachy92@gmail.com>
2025-08-27 11:50:30 -0600

    Add files via upload

---
+++ APIStock.Infrastructure/DependecyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using APIStock.Infrastructure.Repositories;
+using APIStock.Core.Interfaces;
+
+
+namespace APIStock.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionString)
+        {
+            // Registramos IStockRepository con StockRepository
+            services.AddScoped<IStockRepository>(sp => new StockRepository(connectionString));
+            return services;
+        }
+    }
+}
+
