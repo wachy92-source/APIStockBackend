676cc939fff91786a82aa5b4fcab847b0895c47f
wachy92-source <wachy92@gmail.com>
2025-08-27 12:02:27 -0600

    Add files via upload

---
+++ APIStock.Application/DependecyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using APIStock.Core.Entities;
+using APIStock.Core.Interfaces;
+
+
+namespace APIStock.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<StockService>(); // Inyectable
+            return services;
+        }
+    }
+}
+
+
---
+++ APIStock.Application/services.cs
@@ -0,0 +1,25 @@
+﻿using APIStock.Core.Entities;
+using APIStock.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIStock.Application
+{
+    public class StockService
+    {
+        private readonly IStockRepository _repository;
+
+        public StockService(IStockRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<StockItem>> GetStockAsync()
+        {
+            return _repository.GetStockAsync();
+        }
+    }
+}
